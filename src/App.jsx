1803322df618eee5e65b6f3b21f7789ade45850e
kumarbaberwal <itsnitesh16@gmail.com>
2026-01-04 21:33:15 +0530

    Navbar created and pages added and frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { SignInButton } from "@clerk/clerk-react"
+import React from 'react'
+import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
+import EditProductPage from "./pages/EditProductPage"
+import CreatePage from "./pages/CreatePage"
+import ProfilePage from "./pages/ProfilePage"
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/edit/:id" element={<EditProductPage />} />
+
+        </Routes>
+      </main>
+    </div>
+  )
+}
